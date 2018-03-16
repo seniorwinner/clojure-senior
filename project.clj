1041badc3db8b1b8cc6ed335de2a4e6e2f07098e
seniorwinner <alexandr.surovtsev.vrn@yandex.ru>
2018-03-16 12:08:30 +0300

    prepare for openshift

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
-(defproject clojure-senior "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot clojure-senior.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+(def project clojureapp "0.0.1-SNAPSHOT"
+    :dependencies [[org.clojure/clojure "1.5.1"]
+                   [http-kit "2.1.12"]]
+    :main webapp)
+
