3682c416c0c78d451dd6ce55634fbc594a6fdf2f
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2018-03-16 12:02:50 +0300

    template project

---
+++ test/clojure_senior/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-senior.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-senior.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
